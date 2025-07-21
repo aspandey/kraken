bde665bb340555a912f36c4fa8abafd4354b21e0
Ashish Pandey <aashishpandey2014@gmail.com>
2025-07-21 10:10:41 +0530

    Add Dockerfile

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     message = os.environ.get('GREETING_MESSAGE', 'Hello from Flask!')
-    return f"<h1>{message}</h1><p>Running on port {os.environ.get('PORT', '5000')}</p>"
+    return f"<h1>{message}</h1><p>Running on port {os.environ.get('PORT', '5050')}</p>"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5050))
     app.run(debug=True, host='0.0.0.0', port=port)
